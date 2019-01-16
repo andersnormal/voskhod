f8d990d0926955e1514c24d7de8740c196ba40cc
Sebastian DÃoell <sebastian@katallaxie.me>
2019-01-17 00:47:44 +0100

    chore ~ embedding etcd, fixing signals

--- server/etcd/start.go
+++ server/etcd/start.go
@@ -2,6 +2,7 @@ package etcd
 
 import (
 	"go.etcd.io/etcd/embed"
+	"net/url"
 )
 
 // Start is starting the queue
@@ -9,8 +10,14 @@ func (s *server) Start() func() error {
 	return func() error {
 		var err error
 
+		u, err := url.Parse("unix:///var/run/etcd")
+		if err != nil {
+			return err
+		}
+
 		cfg := embed.NewConfig()
 		cfg.Dir = s.cfg.EtcdFilestoreDir()
+		cfg.LCUrls = []url.URL{*u}
 
 		etcd, err := embed.StartEtcd(cfg)
 		if err != nil {
