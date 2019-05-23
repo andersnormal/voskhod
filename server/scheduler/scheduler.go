b8155e2a289d9b403bef33c692ac213877b871e1
Sebastian DÃoell <sebastian@katallaxie.me>
2019-05-23 22:04:45 +0200

    wip: topics

--- server/scheduler/scheduler.go
+++ server/scheduler/scheduler.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/andersnormal/voskhod/agent/config"
+	pb "github.com/andersnormal/voskhod/proto"
 	"github.com/andersnormal/voskhod/server/nats"
 
 	"github.com/nats-io/stan.go"
@@ -19,6 +20,10 @@ func New(nats nats.Nats, opts ...Opt) Scheduler {
 	s.opts = options
 	s.nats = nats
 
+	s.exit = make(chan struct{}, 0)
+	s.in = make(chan pb.Event)
+	s.out = make(chan pb.Event)
+
 	configure(s, opts...)
 
 	return s
@@ -30,13 +35,6 @@ func WithConfig(c *config.Config) func(o *Opts) {
 		o.Config = c
 	}
 }
-
-// // WithNats ...
-// func WithNats(sc stan.Conn) func(o *Opts) {
-// 	return func(o *Opts) {
-// 		o.Nats = sc
-// 	}
-// }
 
 // Stop ...
 func (s *scheduler) Stop() error {
@@ -55,16 +53,14 @@ func (s *scheduler) Start(ctx context.Context) func() error {
 			return err
 		}
 
-		// Simple Synchronous Publisher
-		sc.Publish("foo", []byte("Hello World")) // does not return until an ack has been received from NATS Streaming
+		s.conn = sc
 
-		// Simple Async Subscriber
-		sub, _ := sc.Subscribe("foo", func(m *stan.Msg) {
-			fmt.Printf("Received a message: %s\n", string(m.Data))
-		})
+		// start subscriptions
+		s.run(s.publishEvents)
+		s.run(s.subscribeEvents)
 
-		// Unsubscribe
-		sub.Unsubscribe()
+		// wait ... :)
+		<-s.exit
 
 		// Close connection
 		sc.Close()
@@ -73,7 +69,51 @@ func (s *scheduler) Start(ctx context.Context) func() error {
 	}
 }
 
-// handle ...
+func (s *scheduler) subscribeEvents() error {
+	// Simple Async Subscriber
+	sub, _ := s.conn.Subscribe("events", func(m *stan.Msg) {
+		fmt.Printf("Received an event: %s\n", string(m.Data))
+	})
+
+	// Unsubscribe
+	defer sub.Unsubscribe()
+
+	// wait for the ca
+	<-s.exit
+
+	// noop
+	return nil
+}
+
+func (s *scheduler) publishEvents() error {
+	ticker := time.Tick(5 * time.Second)
+
+	for {
+		select {
+		case <-ticker:
+			// Simple Synchronous Publisher
+			s.conn.Publish("events", []byte("Hello World")) // does not return until an ack has been received from NATS Streaming
+		case <-s.exit:
+			return nil
+		}
+	}
+}
+
+func (s *scheduler) run(fn func() error) {
+	s.wg.Add(1)
+
+	go func() {
+		defer s.wg.Done()
+
+		if err := fn(); err != nil {
+			s.errOnce.Do(func() {
+				s.err = err
+
+				close(s.exit)
+			})
+		}
+	}()
+}
 
 func configure(s *scheduler, opts ...Opt) error {
 	for _, o := range opts {
--- server/scheduler/types.go
+++ server/scheduler/types.go
@@ -1,9 +1,14 @@
 package scheduler
 
 import (
+	"sync"
+
 	"github.com/andersnormal/pkg/server"
 	"github.com/andersnormal/voskhod/agent/config"
+	pb "github.com/andersnormal/voskhod/proto"
 	"github.com/andersnormal/voskhod/server/nats"
+
+	"github.com/nats-io/stan.go"
 )
 
 type Scheduler interface {
@@ -12,6 +17,16 @@ type Scheduler interface {
 
 type scheduler struct {
 	nats nats.Nats
+	conn stan.Conn
+
+	in  chan pb.Event
+	out chan pb.Event
+
+	errOnce sync.Once
+	err     error
+	wg      sync.WaitGroup
+
+	exit chan struct{}
 
 	cfg  *config.Config
 	opts *Opts
